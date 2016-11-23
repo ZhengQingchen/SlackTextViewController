58190a3bccb84b72b9b6a13fef4a87e33a0e4fac
Zheng Jie <zj@ZhengdeMacBook-Pro.local>
2016-11-23 21:52:47 +0800

    add button as FirstResponse

---
+++ Source/UIButtonAsFirstResponder.h
@@ -0,0 +1,13 @@
+//
+//  UIButtonAsFirstResponder.h
+//  Pods
+//
+//  Created by Zheng Jie on 2016/11/23.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButtonAsFirstResponder : UIButton
+
+@end
---
+++ Source/UIButtonAsFirstResponder.m
@@ -0,0 +1,21 @@
+//
+//  UIButtonAsFirstResponder.m
+//  Pods
+//
+//  Created by Zheng Jie on 2016/11/23.
+//
+//
+
+#import "UIButtonAsFirstResponder.h"
+
+@implementation UIButtonAsFirstResponder
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
